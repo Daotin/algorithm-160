261a9e3d8efe1d7d4f1ceda20c20977c6fcff2ef
Daotin <daotin@qq.com>
2025-06-23 07:54:46 +0800

    update

--- 栈.js
+++ 栈.js
@@ -210,4 +210,92 @@ var evalRPN = function (tokens) {
 };
 
 // console.log(evalRPN(["2", "1", "+", "3", "*"]));
-console.log(evalRPN(["4", "13", "5", "/", "+"]));
+// console.log(evalRPN(["4", "13", "5", "/", "+"]));
+
+/**
+ * @param {number} n
+ * @return {string[]}
+22. 括号生成
+
+数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+
+示例 1：
+
+输入：n = 3
+输出：["((()))","(()())","(())()","()(())","()()()"]
+示例 2：
+
+输入：n = 1
+输出：["()"]
+
+
+解题思路：
+这个问题可以通过回溯算法（Backtracking）来解决。回溯算法是一种通过尝试搜索所有可能的候选解来寻找问题解的算法，当发现当前路径无法达到解时，就“回溯”并尝试其他路径。
+对于括号生成问题，我们可以遵循以下规则：
+1. 在任何时刻，左括号的数量都不能少于右括号的数量；
+2. 只有在左括号数量大于右括号数量时，才能添加右括号；
+3. 当左右括号的数量都达到n时，我们就找到了一组有效的括号组合。
+基于上述规则，我们可以设计如下算法：
+1. 初始化一个空字符串作为当前的括号组合；
+2. 使用递归函数，传入当前的括号组合，以及剩余可以添加的左括号和右括号的数量；
+3. 在递归函数中，如果剩余的左括号数量大于0，那么我们可以添加一个左括号，并递归调用自身，减少一个左括号的数量；
+4. 如果剩余的右括号数量大于当前左括号的数量，那么我们可以添加一个右括号，并递归调用自身，减少一个右括号的数量；
+5. 当左括号和右括号的数量都为0时，我们找到了一组有效的括号组合，将其加入到结果列表中；
+6. 最后返回结果列表。
+ */
+var generateParenthesis = function (n) {
+  let str = "";
+  let result = [];
+  function bracket(str = "", left = 0, right = 0) {
+    if (left > 0) {
+      bracket(str + "(", left - 1, right);
+    }
+    if (left < right) {
+      bracket(str + ")", left, right - 1);
+    }
+    if (left == 0 && right == 0) {
+      result.push(str);
+      str = "";
+    }
+  }
+  bracket(str, n, n);
+  return result;
+};
+
+// console.log(generateParenthesis(1));
+// console.log(generateParenthesis(2));
+// console.log(generateParenthesis(3));
+
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+739. 每日温度
+
+给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+
+示例 1:
+输入: temperatures = [73,74,75,71,69,72,76,73]
+输出: [1,1,4,2,1,1,0,0]
+示例 2:
+输入: temperatures = [30,40,50,60]
+输出: [1,1,1,0]
+ */
+var dailyTemperatures = function (temperatures) {
+  const answer = new Array(temperatures.length).fill(0);
+  const stack = []; // 单调递减栈，存储索引
+
+  for (let i = 0; i < temperatures.length; i++) {
+    while (
+      stack.length > 0 &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      const idx = stack.pop();
+      answer[idx] = i - idx;
+    }
+    stack.push(i);
+  }
+
+  return answer;
+};
+
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]));
