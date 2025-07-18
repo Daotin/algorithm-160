46af9a5c4d395a3b620fc3984a4a5be95f8e9dd5
冯道廷 <fengdaoting@whty.com.cn>
2025-07-18 11:30:03 +0800

    修改文件名称

--- 4滑动窗口.js
+++ 4滑动窗口.js
@@ -85,7 +85,59 @@ var lengthOfLongestSubstring = function (s) {
   return maxLength;
 };
 
-console.log(lengthOfLongestSubstring('abcabcbb'));
-console.log(lengthOfLongestSubstring('bbbbbbbb'));
-console.log(lengthOfLongestSubstring('pwwkew'));
-console.log(lengthOfLongestSubstring('abba'));
+// console.log(lengthOfLongestSubstring('abcabcbb'));
+// console.log(lengthOfLongestSubstring('bbbbbbbb'));
+// console.log(lengthOfLongestSubstring('pwwkew'));
+// console.log(lengthOfLongestSubstring('abba'));
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+424. 替换后的最长重复字符
+
+给你一个字符串 s 和一个整数 k 。你可以选择字符串中的任一字符，并将其更改为任何其他大写英文字符。该操作最多可执行 k 次。
+
+在执行上述操作后，返回 包含相同字母的最长子字符串的长度。
+
+ 
+示例 1：
+
+输入：s = "ABAB", k = 2
+输出：4
+解释：用两个'A'替换为两个'B',反之亦然。
+示例 2：
+
+输入：s = "AABABBA", k = 1
+输出：4
+解释：
+将中间的一个'A'替换为'B',字符串变为 "AABBBBA"。
+子串 "BBBB" 有最长重复字母, 答案为 4。
+可能存在其他的方法来得到同样的结果。
+ */
+var characterReplacement = function (s, k) {
+  let left = 0,
+    right = 0,
+    maxCount = 0,
+    maxLength = 0;
+  const map = {};
+
+  while (right < s.length) {
+    map[s[right]] = (map[s[right]] || 0) + 1;
+    maxCount = Math.max(maxCount, map[s[right]]);
+
+    if (right - left + 1 > maxCount + k) {
+      map[s[left]] -= 1;
+      left++;
+    }
+
+    maxLength = Math.max(maxLength, right - left + 1);
+
+    right++;
+  }
+
+  return maxLength;
+};
+
+console.log(characterReplacement('ABAB', 3));
+console.log(characterReplacement('AABABBAAAA', 1));
